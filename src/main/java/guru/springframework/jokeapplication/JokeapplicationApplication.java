8c69a0913761480e3af500d65f242769c72668b9
mnixdorf <35327832+mnixdorf@users.noreply.github.com>
2023-01-02 09:08:34 +0100

    chuck norris quote generator initial commit with spring service

---
+++ src/main/java/guru/springframework/jokeapplication/JokeapplicationApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.jokeapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokeapplicationApplication.class, args);
+	}
+
+}
---
+++ src/main/java/guru/springframework/jokeapplication/services/JokesService.java
@@ -0,0 +1,9 @@
+package guru.springframework.jokeapplication.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+
+public interface JokesService {
+    String getJoke();
+}
+
